abebecaf14a90f028a2ee847945eb8a88e474ced
Welington Rodrigues Braga <welrbraga@yahoo.com>
2020-11-20 08:38:13 -0300

    Remodela as funções de instalação e replicação
    * Cria uma nova função dot_custom_repo() responsável por fazer a
    configuração do repositório local com os valores definidos em dotfiles.conf
    
    * Reestrutura as funções dot_install e dot_replicate:
     - Ambas usam a nova função dot_custom_repo()
     - O código de ambas foi remodelado para que fiquem similares
     - Não é mais usado um symlink para carregar o arquivo dotfiles_manager.bash
       Agora o arquivo original copiado para a pasta local dotfiles-tools é
       invocado diretamente.

--- dotfiles-tools/dotfiles_install.bash
+++ dotfiles-tools/dotfiles_install.bash
@@ -21,25 +21,31 @@ source $HOME/dotfiles-tools/dotfiles.conf
 
 alias dotfile='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
+dot_custom_repo() {
+	# Customize your repo info with dotfiles.conf values
+
+	cd "$HOME/.dotfiles"
+	#Personaliza o repositório
+	echo "Dotfiles de $FULLNAME">$HOME/.dotfiles/description
+	dotfile config --local status.showUntrackedFiles no
+	dotfile config --local user.email "$MAILADDRESS"
+	dotfile config --local user.name "$FULLNAME"
+	dotfile config --local core.hooksPath $HOME/dotfiles-tools/hooks
+	cd "$HOME"
+}
+
 dot_install() {
     #Before use this, certify yourself of you have a private repo in Github
     #to keep your dotfiles safe.
 
     #Cria o repo local
     git init --bare $HOME/.dotfiles
+    if [ ! $? ]; then
+        echo "Não foi possível clonar seu repositório de dotfiles"
+        return
+    fi
 
-    #Personaliza o repositório
-    echo "Dotfiles de $FULLNAME">$HOME/.dotfiles/description
-    dotfile config --local status.showUntrackedFiles no
-    dotfile config --local user.email "$MAILADDRESS"
-    dotfile config --local user.name "$FULLNAME"
-    dotfile config --local core.hooksPath $HOME/dotfiles-tools/hooks
-
-    #Cria um link simbólico para o arquivo com funções de gerencia de dotfiles
-    ln -fs "$PWD/dotfiles_manager.bash" "$HOME/.dotfiles_manager.bash"
-
-    #Configura a inicialização automatica da gerencia dos dotfiles
-    echo "source $HOME/.dotfiles_manager.bash" >> $HOME/.bashrc
+    dot_custom_repo
 
     #Realiza um commit inicial para permitir o push para o servidor remoto
     echo "# dotfiles" >> README.md
@@ -52,8 +58,11 @@ dot_install() {
     dotfile push --mirror git@github.com:${GITHUB_LOGIN}/${GITHUB_REPO}.git
     dotfile push --set-upstream origin master
 
+		#Configura a inicialização automatica da gerencia dos dotfiles
+    echo "source $HOME/dotfiles-tools/dotfiles_manager.bash" >> $HOME/.bashrc
+
     #Carrega as funções de gerência
-    source $HOME/.dotfiles_manager.bash
+    source $HOME/dotfiles-tools/dotfiles_manager.bash
 }
 
 
@@ -61,32 +70,21 @@ dot_replicate() {
 
     #Clone do repo no local
     git clone --bare git@github.com:${GITHUB_LOGIN}/${GITHUB_REPO}.git "$HOME/.dotfiles"
-
     if [ ! $? ]; then
         echo "Não foi possível clonar seu repositório de dotfiles"
         return
     fi
 
-    cd "$HOME/.dotfiles"
-    #Personaliza o repositório
-    echo "Dotfiles de $FULLNAME">$HOME/.dotfiles/description
-    dotfile config --local status.showUntrackedFiles no
-    dotfile config --local user.email "$MAILADDRESS"
-    dotfile config --local user.name "$FULLNAME"
-    dotfile config --local core.hooksPath $HOME/dotfiles-tools/hooks
-    cd "$HOME"
+    dot_custom_repo
 
     #Pega do repositorio somente os arquivos de gerencia
     dotfile reset HEAD
     #dotfile checkout -- dotfiles/**
     dotfile push --set-upstream origin master
 
-    #Cria um link simbólico para o arquivo com funções de gerencia de dotfiles
-    ln -fs "$PWD/dotfiles-tools/dotfiles_manager.bash" "$HOME/.dotfiles_manager.bash"
-
     #Configura a inicialização automatica da gerencia dos dotfiles
-    echo "source $HOME/.dotfiles_manager.bash" >> $HOME/.bashrc
+    echo "source $HOME/dotfiles-tools/dotfiles_manager.bash" >> $HOME/.bashrc
 
     #Carrega as funções de gerência
-    source $HOME/.dotfiles_manager.bash
+    source $HOME/dotfiles-tools/dotfiles_manager.bash
 }
