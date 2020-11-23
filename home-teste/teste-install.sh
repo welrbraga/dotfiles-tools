5bff3314c824f97615d11c75f6c70f1b20e69cfb
Welington Rodrigues Braga <welrbraga@yahoo.com>
2020-11-23 13:10:48 -0300

    Reescrita do script de instalação
    Obtem os arquivos necessários diretamente do repositório no Github

--- home-teste/teste-install.sh
+++ home-teste/teste-install.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# Para testes locais não necessitarem download da versão no repositório Git
-# echo "**clone"
-# git clone http://github.com/welrbraga/dotfiles-tools.git
-# echo "**cp"
-# cp -r dotfiles-tools/dotfiles-tools $HOME
+echo "**Obtem o manager"
+curl --silent 'https://raw.githubusercontent.com/welrbraga/dotfiles-tools/master/dotfiles-tools/dotfiles_manager.bash' -o /tmp/dotfiles_manager.bash
 
-echo "**source"
-source "$HOME/dotfiles-tools/dotfiles_install.bash"
+echo "** Carrega o manager"
+source "/tmp/dotfiles_manager.bash"
 
-echo "**install"
-dot_install
+echo "** Pega pacote completo"
+dot_update
+
+source $HOME/dotfiles-tools/dotfiles_install.bash
+
+#Choose one action
+#dot_replicate #Para usar um repositório de dotfiles já preenchido
+#dot_install #Para usar um repositório de dotfiles VAZIO
