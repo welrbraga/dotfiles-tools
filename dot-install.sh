fa7e2a76131254d7d0bfbc59560fc3cfea975fb9
Welington Rodrigues Braga <welrbraga@yahoo.com>
2021-10-07 20:32:21 -0300

    Adequações conforme shellcheck

--- dot-install.sh
+++ dot-install.sh
@@ -4,12 +4,13 @@ echo "**Obtem o manager"
 curl --silent 'https://raw.githubusercontent.com/welrbraga/dotfiles-tools/master/dotfiles-tools/dotfiles_manager.bash' -o /tmp/dotfiles_manager.bash
 
 echo "** Carrega o manager"
+# shellcheck disable=SC1091
 source "/tmp/dotfiles_manager.bash"
 
 echo "** Pega pacote completo"
 dot_update
 
-source $HOME/dotfiles-tools/dotfiles_install.bash
+source "$HOME/dotfiles-tools/dotfiles_install.bash"
 
 #Para usar um repositório de dotfiles já preenchido
 dot_replicate
@@ -17,6 +18,6 @@ dot_replicate
 #Restaura os arquivos marcados como "Deleted" pelo Git
 #com os seus dotfiles acabaram de ser recuperados do repositório,
 #Estes arquivos são na verdade aqueles não existem ainda na sua cópia local
-for file in `dotfile status --short | awk '$1=="D" { print $2 }'`; do dot-undo $file; done
+for file in $(dotfile status --short | awk '$1=="D" { print $2 }'); do dot-undo "$file"; done
 
 #dot_install #Para usar um repositório de dotfiles VAZIO
